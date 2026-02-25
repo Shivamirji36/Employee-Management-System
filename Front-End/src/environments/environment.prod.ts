0d7fb8d4c4b3f14c30ba9bd0d876799329ee41b4
Shivamirji36 <shivmirji36@icloud.com>
2026-02-26 00:37:43 +0530

    Fix JPQL join for address

--- Front-End/src/environments/environment.prod.ts
+++ Front-End/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://employee-management-system-dvsa.onrender.com'
+  apiUrl: 'https://employee-management-system-dvsa.onrender.com/'
 };
